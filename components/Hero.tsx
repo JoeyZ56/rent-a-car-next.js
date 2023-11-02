1e768e8e87a39578fd8ad6b502fe6f5b19bbf2ad
username <Jzazzi56@gmail.comn>
2023-11-02 15:06:17 -0700

    creating a car rental app using rapidapi

---
+++ components/Hero.tsx
@@ -0,0 +1,30 @@
+"use client";
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero_title">Find, Book, or Book a Car Today</h1>
+        <p className="hero__subtitle">
+          Stream line your car rental experience with out booking process
+        </p>
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ types/types.ts
@@ -0,0 +1,31 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomeButtonProps {
+  title: string;
+  containerStyles?: String;
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
+  btnType?: "button" | "submit";
+  textStyles?: String;
+  rightIcon?: string;
+  isDisabled?: boolean;
+}
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+}
+
+export interface carProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
